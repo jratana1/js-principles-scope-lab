f17ce6c8168cd04db1a4b93a83b78fe5a6db2dfb
John Ratana <jratana1@gmail.com>
2021-01-26 20:39:00 -0500

    commit tree 22fc5dcd83c78b8bd3cd6eabba8f8beeb4f432fa

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+
+function setBestCustomer(){
+    bestCustomer = 'not bob'
+    
+}
+
+function overwriteBestCustomer(input){
+    bestCustomer = input
+}
+
+const leastFavoriteCustomer = 'Hill'
+
+function changeLeastFavoriteCustomer(){
+    leastFavoriteCustomer = 'bob'
+}
